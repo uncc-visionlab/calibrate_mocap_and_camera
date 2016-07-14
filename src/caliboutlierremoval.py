04b0b1d32bfcdf6077d48ed13f1dfab6f02dc3c9
arwillis <arwillis@arwillis-laptop1>
2016-07-14 13:13:05 -0500

    Updated caliboutlierremoval.py to improve usability.

--- src/caliboutlierremoval.py
+++ src/caliboutlierremoval.py
@@ -1,10 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
 import math
 import matplotlib.pyplot as plt
-from os import environ
-from sys import version_info
-
-if version_info[0] < 3:
-    raise "Must be using Python 3"
 
 def quatertoRPY(x,y,z,w):
     roll = math.atan2(2.0*(x*w + y*z), 1 - 2*(z*z + w*w))
@@ -19,25 +18,26 @@ def quatertoRPY(x,y,z,w):
 
 def removeoutliers(poseparam, ulimit=None, llimit=None):
     newposeparam = []
+    
     for i, value in enumerate(poseparam):
         if value is None:
-            newposeparam.append(None)
-        elif ulimit is not None and value > ulimit:
-            newposeparam.append(None)
-            if '%' not in strdata[i][0]:
-                strdata[i][0] = '%' + strdata[i][0]
-        elif llimit is not None and value < llimit:
-            newposeparam.append(None)
-            if '%' not in strdata[i][0]:
-                strdata[i][0] = '%' + strdata[i][0]
+            newposeparam.append(value)
+        elif ulimit is not None and value > ulimit :
+                newposeparam.append(None)
+                if '%' not in strdata[i][0]:
+                    strdata[i][0] = '%' + strdata[i][0]
+        elif llimit is not None  and value < llimit :
+                newposeparam.append(None)
+                if '%' not in strdata[i][0]:
+                    strdata[i][0] = '%' + strdata[i][0]
         else:
             newposeparam.append(value)
     return(newposeparam)
 
 pi = math.pi
 
-inputfile = open(environ["HOME"] + '/calib_transforms.txt', 'r')
-outputfile = open(environ["HOME"] + '/calib_transforms_no_outliers.m', 'w')
+inputfile = open('/home/arwillis/calib_transforms_17767.txt', 'r')
+outputfile = open('/home/arwillis/calib_transforms_no_outliers.m', 'w')
 
 strdata = [line.strip("\n").split(" ") for line in inputfile.readlines()[1:]]
 data = [[float(value) for value in row] for row in strdata]
@@ -59,14 +59,8 @@ for row in data:
     pitch.append(pit*180/pi)
     yaw.append(ya*180/pi)
 
-exit = False
-
-plt.interactive('True')
-fig = plt.figure()
 while(exit != True):
     
-    
-    fig.clear()
     plt.subplot(3, 2, 1)
     plt.plot(range(len(x)), x, "b-")
     plt.ylabel("x")
@@ -97,11 +91,11 @@ while(exit != True):
     plt.ylabel("roll")
     plt.xlabel("Measurement Index")
     
-    plt.draw()
+    plt.show()
     
-    userparam = input("Select the parameter you would like to limit (x, y, z, yaw, pitch, roll) or \"exit\" to exit:\n")
+    userparam = input("Select the parameter you would like to limit (x, y, z, ya, pi, ro) or \"exit\" to exit:\n")
 
-    if userparam in ['x', 'y', 'z', 'yaw', 'pitch', 'roll']:
+    if userparam in ['x', 'y', 'z', 'ya', 'pi', 'ro']:
         upper = input("Select an upper limit for the data in " + userparam + " (or return for no limit):\n")
         lower = input("Select a lower limit for the data in " + userparam + " (or return for no limit):\n")
         
@@ -119,13 +113,13 @@ while(exit != True):
         elif userparam == 'z':
             z = removeoutliers(z, upper, lower)
         
-        elif userparam == 'roll':
+        elif userparam == 'ro':
             roll = removeoutliers(roll, upper, lower)
             
-        elif userparam == 'pitch':
+        elif userparam == 'pi':
             pitch = removeoutliers(pitch, upper, lower)
         
-        elif userparam == 'yaw':
+        elif userparam == 'ya':
             yaw = removeoutliers(yaw, upper, lower)            
     
     elif userparam == "exit":
@@ -140,7 +134,7 @@ for row in strdata:
 
 outputfile.write("]")
 
-print("Changes written to: " + outputfile.name)
+print("Changes written to: "+outputfile.name)
 
 inputfile.close()
 outputfile.close()
